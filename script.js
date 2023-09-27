8b5a951a3f6f3c9556c50a78401e47bc7005f9ec
Nii~ Nii~ <naveenbhandari150@gmail.com>
2023-09-27 09:09:33 -0700

    added chat feature

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function displayNames() {
                         console.log(r.summary);
                         let a= document.createElement("div");
                         a.innerHTML = r.summary;
-                        a.setAttribute("style", "width: 650px; color: beige; background: purple; border-radius:5px; padding:5px")
+                        a.setAttribute("style", "width: 650px; color: beige; background: purple; border-radius:15px; padding:20px")
                         box.appendChild(a);
                     })
                 }).catch(err=>{
@@ -63,6 +63,32 @@ function displayNames() {
   }
 
 }
+
+const bt=  document.getElementById("submit")
+bt.addEventListener("click", ()=>{
+  let text = document.getElementById("input").value;
+  if (!text) return;
+  const box = document.getElementsByClassName("chatbox")[0]
+  let bubble = document.createElement("div");
+  bubble.innerHTML = text;
+  bubble.setAttribute("style", "width: 650px; color: black; background: cyan; border-radius:15px; padding:20px;")
+  box.appendChild(bubble)
+  fetch("http://127.0.0.1:8000/chat_response/", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body : JSON.stringify({prompt: text}) 
+  }).then(res=>{
+    res.json().then(r=>{
+      let a = document.createElement("div");
+      a.innerHTML = r.reply;
+      a.setAttribute("style", "width: 650px; color: beige; background: purple; border-radius:15px; padding:20px;")
+      box.appendChild(a);
+    })
+  })
+})
+
 
 
 
